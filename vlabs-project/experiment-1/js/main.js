1855d9830719e2862dbb689e6e17ff53c52f34ef
kganeshchandan <kanakala.ganesh@research.iiit.ac.in>
2022-08-23 00:01:50 +0530

    added basic functionalities

--- vlabs-project/experiment-1/js/main.js
+++ vlabs-project/experiment-1/js/main.js
@@ -1,58 +1,54 @@
 // import * as THREE from 'three';
-import { OrbitControls } from 'threeOC';
-import { addSphere , CheckHover, DeleteObject} from './utils.js';
+import { OrbitControls } from "threeOC";
+import {
+    AddLight,
+    addSphere,
+    CheckHover,
+    DeleteObject,
+    TranslatePattern,
+} from "./utils.js";
+
 // import { RectAreaLightHelper } from 'threeRectAreaLightHelper';
 // import { RectAreaLightUniformsLib } from 'threeRectAreaLightUniformsLib';
-
-
 //  init camera
 var camera = new THREE.PerspectiveCamera(
     75, //FOV
-    window.innerWidth/window.innerHeight, //aspect ratio
+    window.innerWidth / window.innerHeight, //aspect ratio
     0.1,
-    1000,
+    1000
 );
-camera.position.set( 10, 10, 20 );
+camera.position.set(10, 10, 20);
 
 // init the renderer and the scene
 var scene = new THREE.Scene();
-var renderer = new THREE.WebGLRenderer({antialias: true});
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#000000");
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
-
 // initialize the axes
 var axesHelper = new THREE.AxesHelper(window.innerHeight);
-scene.add( axesHelper );
-
+scene.add(axesHelper);
 
 // add light to the  system
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(50,100,0);
-light.castShadow = true;
-const light2 = new THREE.DirectionalLight(0xffffff, 1);
-light2.position.set(-50,100,0);
-light2.castShadow = true;
-scene.add(light);
-scene.add(light2);
-
-
-
+const lights = AddLight();
+for (let i = 0; i < lights.length; i++) {
+    scene.add(lights[i]);
+}
 // init the orbit controls
-var controls = new OrbitControls( camera, renderer.domElement );
+var controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 controls.autoRotate = true;
-controls.autoRotateSpeed = 0.5;
+controls.autoRotateSpeed = 0;
 controls.enablePan = false;
 controls.enableDamping = true;
 
 // to check the current object which keyboard points to
 let INTERSECTED;
 
-function getMouseCoords(event){
+function getMouseCoords(event) {
     var mouse = new THREE.Vector2();
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -61,60 +57,107 @@ function getMouseCoords(event){
 var mouse = new THREE.Vector2();
 //  detect mouse click
 let drag = false;
-document.addEventListener("mousedown", function(event){
+document.addEventListener("mousedown", function (event) {
     drag = false;
     mouse = getMouseCoords(event);
-})
-document.addEventListener("mousemove", function(event){
+});
+document.addEventListener("mousemove", function (event) {
     drag = true;
     mouse = getMouseCoords(event);
-})
+});
 
-document.addEventListener("keydown", function(event){
-    var keyCode = event.key ;
-    if (keyCode == 'd'){
+document.addEventListener("keydown", function (event) {
+    var keyCode = event.key;
+    if (keyCode == "d") {
         DeleteObject(mouse, camera, scene, atomList, INTERSECTED);
     }
-})
+});
 
-let action = "addAtom";
+let action = "";
 
 // create a list of atoms in scene
-var atomList = []
-document.addEventListener("mouseup", function(event){
-    if (drag == false){
+var atomList = [];
+
+var SelectAtomList = [];
+// listen to the mouse up
+document.addEventListener("mouseup", function (event) {
+    if (drag == false) {
         // if the action is add atom
-        if (action == "addAtom"){
+        if (action == "addAtom") {
+            if (mouse.y < -0.8) {
+                return;
+            }
             var newSphere = addSphere(mouse, camera, scene);
             scene.add(newSphere);
             atomList.push(newSphere);
         }
-    };
-})
+        if (action == "selectAtom") {
+            INTERSECTED = CheckHover(mouse, camera, atomList, INTERSECTED);
+            if (INTERSECTED) {
+                SelectAtomList.push(INTERSECTED);
+            }
+        }
+    }
+});
 
+// respond to click addAtom
+const addSphereButton = document.getElementById("AddAtom");
+addSphereButton.addEventListener("click", function () {
+    console.log("adding atom mode");
+    if (action != "addAtom") {
+        action = "addAtom";
+        document.getElementById("AddAtom").style =
+            "background-color: rgba(0,255,255,0.75); color: #000000 ";
+    } else {
+        action = "";
+        document.getElementById("AddAtom").style =
+            "color: rgba(127,255,255,0.75);background: transparent; outline: 1px solid rgba(127,255,255,0.75);border: 0px;padding: 5px 10px;cursor: pointer;";
+    }
+});
 
+// respond to select a bunch of atoms
+const addSelectList = document.getElementById("SelectAtom");
+addSelectList.addEventListener("click", function () {
+    console.log("selecting atom mode");
+    if (action != "selectAtom") {
+        action = "selectAtom";
+        document.getElementById("SelectAtom").style =
+            "background-color: rgba(0,255,255,0.75); color: #000000 ";
+    } else {
+        action = "";
+        document.getElementById("SelectAtom").style =
+            "color: rgba(127,255,255,0.75);background: transparent; outline: 1px solid rgba(127,255,255,0.75);border: 0px;padding: 5px 10px;cursor: pointer;";
+    }
+});
 
-
-
-
-
+// respond to translate
+const translateList = document.getElementById("TranslatePattern");
+translateList.addEventListener("click", function () {
+    console.log("translating");
+    var newAtoms = TranslatePattern(SelectAtomList);
+    for (let i = 0; i < newAtoms.length; i++) {
+        scene.add(newAtoms[i]);
+    }
+    SelectAtomList = newAtoms;
+});
 
 // make the window responsive
-window.addEventListener('resize', ()=>{
-    renderer.setSize(window.innerWidth,window.innerHeight);
+window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-})
-
-document.getElementById("WebGL-output").appendChild(renderer.domElement);
-
+});
 
 // render the scene and animate
-var render = function() {
+var render = function () {
+    for (let i = 0; i < SelectAtomList.length; i++) {
+        var curAtom = SelectAtomList[i];
+        console.log(curAtom.position);
+    }
     INTERSECTED = CheckHover(mouse, camera, atomList, INTERSECTED);
     requestAnimationFrame(render);
     controls.update();
-    renderer.render(scene,camera);
-}
+    renderer.render(scene, camera);
+};
 
 render();
--- vlabs-project/experiment-1/js/utils.js
+++ vlabs-project/experiment-1/js/utils.js
@@ -1,8 +1,7 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'threeOC';
+import * as THREE from "three";
+import { OrbitControls } from "threeOC";
 
-
-export function addSphere(mouse, camera, scene){
+export function addSphere(mouse, camera, scene) {
     var intersectionPoint = new THREE.Vector3();
     var planeNormal = new THREE.Vector3();
     var plane = new THREE.Plane();
@@ -12,38 +11,74 @@ export function addSphere(mouse, camera, scene){
     raycaster.setFromCamera(mouse, camera);
     raycaster.ray.intersectPlane(plane, intersectionPoint);
     const sphereMesh = new THREE.Mesh(
-        new THREE.SphereGeometry(1,20,20),
+        new THREE.SphereGeometry(1, 20, 20),
         new THREE.MeshStandardMaterial({
-        color:0xFFEA00,
-        name:"sphere",
-        roughness: 0
+            color: 0x00ffff,
+            name: "sphere",
+            roughness: 0,
         })
     );
     sphereMesh.position.copy(intersectionPoint);
     return sphereMesh;
 }
-export function CheckHover(mouse, camera, atomList, INTERSECTED){
+export function CheckHover(mouse, camera, atomList, INTERSECTED) {
     var raycaster = new THREE.Raycaster();
     raycaster.setFromCamera(mouse, camera);
-    const intersects = raycaster.intersectObjects( atomList, false );
+    const intersects = raycaster.intersectObjects(atomList, false);
 
-    if ( intersects.length > 0 ) {
-        if ( INTERSECTED != intersects[ 0 ].object ) {
+    if (intersects.length > 0) {
+        if (INTERSECTED != intersects[0].object) {
+            if (INTERSECTED)
+                INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
 
-            if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
-
-            INTERSECTED = intersects[ 0 ].object;
+            INTERSECTED = intersects[0].object;
             INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
-            INTERSECTED.material.emissive.setHex( 0xff44ff );
+            INTERSECTED.material.emissive.setHex(0xff44ff);
         }
     } else {
-        if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+        if (INTERSECTED)
+            INTERSECTED.material.emissive.setHex(INTERSECTED.currentHex);
         INTERSECTED = null;
     }
     return INTERSECTED;
 }
 
-export function DeleteObject(mouse, camera, scene, atomList, INTERSECTED){
-    INTERSECTED= CheckHover(mouse, camera, atomList, INTERSECTED);
+export function DeleteObject(mouse, camera, scene, atomList, INTERSECTED) {
+    INTERSECTED = CheckHover(mouse, camera, atomList, INTERSECTED);
     scene.remove(INTERSECTED);
+}
+
+export function AddLight() {
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(100, 100, 50);
+    light.castShadow = true;
+    const light2 = new THREE.DirectionalLight(0xffffff, 1);
+    light2.position.set(-100, 100, -50);
+    light2.castShadow = true;
+    const light3 = new THREE.DirectionalLight(0xffffff, 1);
+    light3.position.set(0, 100, 0);
+    light3.castShadow = true;
+
+    return [light, light2, light3];
+}
+
+export function TranslatePattern(SelectAtomList) {
+    var newAtoms = [];
+    for (let i = 0; i < SelectAtomList.length; i++) {
+        var curAtom = SelectAtomList[i];
+        var newpos = curAtom.position.clone();
+        var translateVec = new THREE.Vector3(1, 1, 1);
+        newpos.add(translateVec);
+        var sphereMesh = new THREE.Mesh(
+            new THREE.SphereGeometry(1, 20, 20),
+            new THREE.MeshStandardMaterial({
+                color: 0x00ffff,
+                name: "sphere",
+                roughness: 0,
+            })
+        );
+        sphereMesh.position.copy(newpos);
+        newAtoms.push(sphereMesh);
+    }
+    return newAtoms;
 }
